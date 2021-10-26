3ec38c6e4fe8b6ea333e1be5ddfc47f3e0c59531
Nicolas Blaisdell <nblaisdell2@gmail.com>
2021-10-25 22:19:41 -0400

    bring tailwind back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
