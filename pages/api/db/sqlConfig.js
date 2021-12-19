9d5c03f291f3fd8db231e926cdf6e8f094b152e4
Nicolas Blaisdell <nblaisdell2@gmail.com>
2021-12-19 17:23:16 -0500

    fixed javascript/react errors v3

--- pages/api/db/sqlConfig.js
+++ pages/api/db/sqlConfig.js
@@ -1,33 +1,33 @@
 var sql = null;
 var sqlConfig = {};
 
-// DEV / LOCALHOST - SQL Server Connection
-// DONT FORGET TO REINSTALL: npm install msnodesqlv8
-sql = require("mssql/msnodesqlv8");
-sqlConfig = {
-  driver: "msnodesqlv8",
-  parseJSON: true,
-  connectionString:
-    "Driver={SQL Server Native Client 11.0};Server=DESKTOP-T76C4AL\\DESKTOPHOME;Database=BudgetHelper;Integrated Security=True;Trusted_Connection=yes;",
-};
-
-// // PRODUCTION / AWS - SQL Server Connection
-// // DONT FORGET TO UNINSTALL: npm uninstall msnodesqlv8
-// sql = require("mssql");
+// // DEV / LOCALHOST - SQL Server Connection
+// // DONT FORGET TO REINSTALL: npm install msnodesqlv8
+// sql = require("mssql/msnodesqlv8");
 // sqlConfig = {
-//   user: "admin",
-//   password: "FG2xnEcDpC8kNLbAfLUw",
-//   database: "BudgetHelper",
-//   server: "sql-server-db-1.ctm8otgitadb.us-east-1.rds.amazonaws.com",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000,
-//   },
-//   options: {
-//     trustServerCertificate: true, // change to true for local dev / self-signed certs
-//   },
+//   driver: "msnodesqlv8",
+//   parseJSON: true,
+//   connectionString:
+//     "Driver={SQL Server Native Client 11.0};Server=DESKTOP-T76C4AL\\DESKTOPHOME;Database=BudgetHelper;Integrated Security=True;Trusted_Connection=yes;",
 // };
+
+// PRODUCTION / AWS - SQL Server Connection
+// DONT FORGET TO UNINSTALL: npm uninstall msnodesqlv8
+sql = require("mssql");
+sqlConfig = {
+  user: "admin",
+  password: "FG2xnEcDpC8kNLbAfLUw",
+  database: "BudgetHelper",
+  server: "sql-server-db-1.ctm8otgitadb.us-east-1.rds.amazonaws.com",
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
 
 // This will run a stored procedure and return the results as JSON
 function query(res, spName, params) {
